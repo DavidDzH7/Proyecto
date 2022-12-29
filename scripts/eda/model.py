cb46e0d625a4afacc3baf3a0eb4244a0a4ba5e60
David Dizes <dizesdavid@hotmail.com>
2022-12-29 08:01:22 -0500

    Se mueve el script del modelo para la carpeta

--- scripts/eda/model.py
+++
@@ -1,7 +0,0 @@
-
-def modelo()
-    clf = MLPClassifier(hidden_layer_sizes=(256,128,64,32)
-                        ,activation="relu"
-                        ,random_state=1).fit(X_train, y_train)
-
-    y_pred=clf.predict(X_test)
--- scripts/training/model.py
+++ scripts/training/model.py
@@ -1,7 +1,7 @@
-from sklearn.neural_network import MLPClassifier
 
-clf = MLPClassifier(hidden_layer_sizes=(256,128,64,32)
-                    ,activation="relu"
-                    ,random_state=1).fit(X_train, y_train)
+def modelo()
+    clf = MLPClassifier(hidden_layer_sizes=(256,128,64,32)
+                        ,activation="relu"
+                        ,random_state=1).fit(X_train, y_train)
 
-y_pred=clf.predict(X_test)
+    y_pred=clf.predict(X_test)
